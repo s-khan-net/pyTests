096e64db0a98d9dc0287fe42009fac09903bc2c9
Saud Khan <saud@saudkhan.net>
2021-07-12 20:28:13 +0530

    Changed

--- sqllite3_json.py
+++ sqllite3_json.py
@@ -4,18 +4,16 @@ import json
 
 movies = json.loads(Path("movies.json").read_text())
 # print(movies[0])
-with sqlite3.connect("db.sqllite3") as conn:
-    command = "INSERT INTO movies valies(?,?,?)"
+with sqlite3.connect("db.sqlite3") as conn:
+    command = "create table movies (id int, title text, year int)"
+    conn.execute(command)
+    command = "INSERT INTO movies values(?,?,?)"
     for movie in movies:
         conn.execute(command, tuple(movie.values()))
     conn.commit()
-
-
-# get https://sqlitebrowser.org/dl/  create table called movies
-#read
-# with sqlite3.connect("db.sqllite3") as conn:
-#     command = "SELECT * from movies"
-#     cursor = conn.execute(command)
-#     for movie in cursor:
-#         print(movie)
-
+# get https://sqliteonline.com/
+# read
+    command = "SELECT * from movies"
+    cursor = conn.execute(command)
+    for movie in cursor:
+        print(movie)
